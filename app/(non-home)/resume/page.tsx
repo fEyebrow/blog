bd54ac066a4525cfcb7c2c4d26db6e43470063bd
四条眉毛 <fourfishxiake@gmail.com>
2023-07-09 17:21:10 +0800

    fix scripts for build and start

--- app/(non-home)/resume/page.tsx
+++ app/(non-home)/resume/page.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <h1 className="font-serif text-4xl font-bold my-4">Resume</h1>
-      <h3 className="text-xl my-4">
-        林锦
-      </h3>
+      <div className="w-full" >WIP</div>
     </>
   );
 }
